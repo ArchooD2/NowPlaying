29c40e826b704c12de345d011b9b6befa832ff65
PJSans <archood2next@gmail.com>
2025-06-30 18:08:43 -0600

    Bump version to 0.1.3 in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="nowplaying",
-    version="0.1.0",
+    version="0.1.3",
     description="CLI tool for playing audio and visualizing waveform with metadata.",
     author="PJ H.",
     author_email="archood2@gmail.com",
